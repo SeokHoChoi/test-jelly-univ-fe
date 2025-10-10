2c34b314c4af572db6bc1bfbaab3718bc1c3776d
최석호 <seokho.choi@ikoob.com>
2025-10-10 12:10:53 +0900

    feat: 분석 페이지 > 인트로 섹션 UI 구현

--- src/components/product-analysis/IntroSection.tsx
+++ src/components/product-analysis/IntroSection.tsx
@@ -1,20 +1,18 @@
-import Text from '@/components/common/Text';
-
 const IntroSection = () => {
   return (
-    <section className="bg-brand-blue-light py-12 md:py-20">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <Text variant="caption" className="text-brand-blue mb-4">
+    <section className="pt-[60px] pb-[40px] md:pt-20 md:pb-10" style={{ backgroundColor: '#003DA5' }}>
+      <div className="mx-auto text-center">
+        <p className="text-[#F2F2F2] font-medium text-[20px] mb-5">
           현재 급여 제품 분석
-        </Text>
+        </p>
 
-        <Text variant="title" className="mb-6 text-gray-900">
-          내가 먹이는 이 사료, 정말 믿고 먹일 수 있을까요?
-        </Text>
+        <h2 className="mb-7 text-[#FFFFFF] font-semibold text-[25px] md:text-[45px]">
+          내가 먹이는 이 사료,<br />정말 믿고 먹일 수 있을까요?
+        </h2>
 
-        <Text variant="subtitle" className="text-gray-700 max-w-3xl mx-auto">
-          서울대·한국수의영양학회 임원 수의사 함께 설계한 AI가 30초 만에 분석해드려요!
-        </Text>
+        <p className="text-[#F8F8F8] font-medium text-[15px] md:text-[28px] mx-auto">
+          서울대·한국수의영양학회 임원 수의사와 함께<br className="md:hidden" /> 설계한 AI가 30초 만에 분석해드려요!
+        </p>
       </div>
     </section>
   );
