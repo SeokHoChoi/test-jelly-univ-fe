31cc8df08d5e7025103b87c7d21493ba4501ce9e
puhuk <wetr235@gmail.com>
2025-10-15 17:28:54 +0900

    feat: Google Analytics 및 Meta Pixel 추적 코드 추가
    - Google Analytics (GA4) 추적 코드 추가 (측정 ID: G-3QZFGDB14S)
    - Meta Pixel (Facebook Pixel) 추적 코드 추가 (Pixel ID: 4182182545371999)
    - PageView 이벤트 자동 추적 설정
    - Next.js Script 컴포넌트 사용으로 성능 최적화 (afterInteractive 전략)
    - noscript 폴백 이미지 추가 (Meta Pixel)
    - ESLint 규칙 예외 처리 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
@@ -35,6 +36,50 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3QZFGDB14S"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3QZFGDB14S');
+          `}
+        </Script>
+
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '4182182545371999');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=4182182545371999&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <AuthProvider>
           <Header />
           <main>{children}</main>
