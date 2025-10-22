b474befcf2e118d9ec27009eb5b260afc1b304d4
puhuk <wetr235@gmail.com>
2025-10-22 12:55:26 +0900

    feat: Add Hotjar and Channel Talk tracking codes
    - Hotjar (Content Square) 추적 코드 추가
    - Channel Talk 실시간 채팅 위젯 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,60 @@ export default function RootLayout({
           />
         </noscript>
 
+        {/* Hotjar Tracking Code (Content Square) */}
+        <Script
+          src="https://t.contentsquare.net/uxa/6d9d3510da5a8.js"
+          strategy="afterInteractive"
+        />
+
+        {/* Channel Talk */}
+        <Script
+          id="channel-talk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var w = window;
+                if (w.ChannelIO) {
+                  return w.console.error("ChannelIO script included twice.");
+                }
+                var ch = function() {
+                  ch.c(arguments);
+                };
+                ch.q = [];
+                ch.c = function(args) {
+                  ch.q.push(args);
+                };
+                w.ChannelIO = ch;
+                function l() {
+                  if (w.ChannelIOInitialized) {
+                    return;
+                  }
+                  w.ChannelIOInitialized = true;
+                  var s = document.createElement("script");
+                  s.type = "text/javascript";
+                  s.async = true;
+                  s.src = "https://cdn.channel.io/plugin/ch-plugin-web.js";
+                  var x = document.getElementsByTagName("script")[0];
+                  if (x.parentNode) {
+                    x.parentNode.insertBefore(s, x);
+                  }
+                }
+                if (document.readyState === "complete") {
+                  l();
+                } else {
+                  w.addEventListener("DOMContentLoaded", l);
+                  w.addEventListener("load", l);
+                }
+              })();
+
+              ChannelIO('boot', {
+                "pluginKey": "35e9c103-78c8-4c3e-98fe-28d1c2fe43c6"
+              });
+            `,
+          }}
+        />
+
         <AuthProvider>
           <Header />
           <main>{children}</main>
