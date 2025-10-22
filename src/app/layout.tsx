fbc2a23f5558edb6f77e7c7504ecdf379b634bca
최석호 <seokho.choi@ikoob.com>
2025-10-22 13:30:57 +0900

    feat: 환경변수로 Hotjar, Channel Talk 설정 변경
    - NEXT_PUBLIC_HOTJAR_ID 환경변수 사용
    - NEXT_PUBLIC_CHANNEL_TALK_KEY 환경변수 사용
    - 프로덕션/개발 환경 모두 지원

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,58 +81,62 @@ export default function RootLayout({
         </noscript>
 
         {/* Hotjar Tracking Code (Content Square) */}
-        <Script
-          src="https://t.contentsquare.net/uxa/6d9d3510da5a8.js"
-          strategy="afterInteractive"
-        />
+        {process.env.NEXT_PUBLIC_HOTJAR_ID && (
+          <Script
+            src={`https://t.contentsquare.net/uxa/${process.env.NEXT_PUBLIC_HOTJAR_ID}.js`}
+            strategy="afterInteractive"
+          />
+        )}
 
         {/* Channel Talk */}
-        <Script
-          id="channel-talk"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                var w = window;
-                if (w.ChannelIO) {
-                  return w.console.error("ChannelIO script included twice.");
-                }
-                var ch = function() {
-                  ch.c(arguments);
-                };
-                ch.q = [];
-                ch.c = function(args) {
-                  ch.q.push(args);
-                };
-                w.ChannelIO = ch;
-                function l() {
-                  if (w.ChannelIOInitialized) {
-                    return;
+        {process.env.NEXT_PUBLIC_CHANNEL_TALK_KEY && (
+          <Script
+            id="channel-talk"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                  var w = window;
+                  if (w.ChannelIO) {
+                    return w.console.error("ChannelIO script included twice.");
                   }
-                  w.ChannelIOInitialized = true;
-                  var s = document.createElement("script");
-                  s.type = "text/javascript";
-                  s.async = true;
-                  s.src = "https://cdn.channel.io/plugin/ch-plugin-web.js";
-                  var x = document.getElementsByTagName("script")[0];
-                  if (x.parentNode) {
-                    x.parentNode.insertBefore(s, x);
+                  var ch = function() {
+                    ch.c(arguments);
+                  };
+                  ch.q = [];
+                  ch.c = function(args) {
+                    ch.q.push(args);
+                  };
+                  w.ChannelIO = ch;
+                  function l() {
+                    if (w.ChannelIOInitialized) {
+                      return;
+                    }
+                    w.ChannelIOInitialized = true;
+                    var s = document.createElement("script");
+                    s.type = "text/javascript";
+                    s.async = true;
+                    s.src = "https://cdn.channel.io/plugin/ch-plugin-web.js";
+                    var x = document.getElementsByTagName("script")[0];
+                    if (x.parentNode) {
+                      x.parentNode.insertBefore(s, x);
+                    }
                   }
-                }
-                if (document.readyState === "complete") {
-                  l();
-                } else {
-                  w.addEventListener("DOMContentLoaded", l);
-                  w.addEventListener("load", l);
-                }
-              })();
+                  if (document.readyState === "complete") {
+                    l();
+                  } else {
+                    w.addEventListener("DOMContentLoaded", l);
+                    w.addEventListener("load", l);
+                  }
+                })();
 
-              ChannelIO('boot', {
-                "pluginKey": "35e9c103-78c8-4c3e-98fe-28d1c2fe43c6"
-              });
-            `,
-          }}
-        />
+                ChannelIO('boot', {
+                  "pluginKey": "${process.env.NEXT_PUBLIC_CHANNEL_TALK_KEY}"
+                });
+              `,
+            }}
+          />
+        )}
 
         <AuthProvider>
           <Header />
